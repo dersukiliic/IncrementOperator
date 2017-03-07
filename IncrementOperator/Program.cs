90ad73650b4ef71c51595fbc2c05a1394002d520
JuneCho23 <junecho.ucb@gmail.com>
2017-03-07 08:13:31 -0800

    Increment Operator Program

---
+++ IncrementOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IncrementOperator
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int n1 = 0;
+			int n2 = 0;
+			int n3 = 0;
+
+			Console.WriteLine($"{n1},  {n2},  {n3}");
+			n1 = 5;
+			Console.WriteLine($"{n1},  {n2},  {n3}");
+			n2 = n1++;
+			Console.WriteLine($"{n1},  {n2},  {n3}");
+			n3 = ++n1;
+			Console.WriteLine($"{n1},  {n2},  {n3}");
+
+
+
+		}
+	}
+}
